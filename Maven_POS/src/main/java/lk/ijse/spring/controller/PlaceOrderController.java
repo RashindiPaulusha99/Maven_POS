dab1eec83412c4ddf900c94fae2029e0ea4682c9
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2022-07-03 12:26:43 +0530

    add orderdto

--- Maven_POS/src/main/java/lk/ijse/spring/controller/PlaceOrderController.java
+++ Maven_POS/src/main/java/lk/ijse/spring/controller/PlaceOrderController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("order")
 @CrossOrigin
-public class OrderController {
+public class PlaceOrderController {
 
 }
---
+++ Maven_POS/src/main/java/lk/ijse/spring/dto/OrderDTO.java
@@ -0,0 +1,24 @@
+package lk.ijse.spring.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class OrderDTO {
+    private String orderId;
+    private CustomerDTO customer;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate orderDate;
+    private double grossTotal;
+    private double netTotal;
+    List<OrderDetailsDTO> orderDetails;
+}
